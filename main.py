698ba9855ec4d1d48d7b68f2c72f0b63ca11ea37
Лоторев Кирилл <151776260+KirillCodeCreator@users.noreply.github.com>
2023-12-14 22:26:26 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
 import sys
 import random
+from UI import Ui_MainWindow
 
 
-class Example(QMainWindow):
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('untitled.ui', self)
-        self.show()
+        self.setupUi(self)
         self.flag = False
-        self.setWindowTitle('Git и желтые окружности')
+        self.setWindowTitle('Git и случайные окружности')
         self.pushButton.clicked.connect(self.draw)
         self.coords = []
 
     def draw(self):
         self.figure = 'circle'
         self.size = random.randint(10, 100)
-        self.color = [255, 255, 0]
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         self.flag = True
         self.update()
 
@@ -28,19 +27,19 @@ class Example(QMainWindow):
             qp.begin(self)
             qp.setPen(QColor(*self.color))
             qp.setBrush(QColor(*self.color))
-            self.x, self.y = random.randint(100, [750, 500][0] - 100), random.randint(100, [750, 500][1] - 100)
+            self.x, self.y = random.randint(100, [500, 500][0] - 100), random.randint(100, [500, 500][1] - 100)
             if self.figure == 'circle':
                 qp.drawEllipse(self.x, self.y, self.size, self.size)
             qp.end()
 
 
-def start(cls, exception, traceback):
+def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    sys.excepthook = start
+    sys.excepthook = except_hook
     ex = Example()
     ex.show()
     sys.exit(app.exec_())
